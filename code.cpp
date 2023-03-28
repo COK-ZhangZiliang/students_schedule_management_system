1751c112300ede923f2e94b0bf461e69181a5996
杨晨 <1369792882@qq.com>
2023-03-28 12:35:08 +0000

    测试

--- code.cpp
+++ code.cpp
@@ -1,7 +1,11 @@
 #include<bits/stdc++.h>
+#include <ostream>
 using namespace std;
 
 int main()
 {
-	cout<<"1"<<endl;
+	cout<<"1"<<endl;、
+	cout << "zzl天下第一！" << std::endl;
+	system("pause");
+	return 0;
 }
