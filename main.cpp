29059e962d3ac9984a5b876554e486d486c54973
Steve_ZL <1907973434@qq.com>
2023-04-30 23:44:39 +0800

    添加dijkstra算法

--- main.cpp
+++ main.cpp
@@ -4,6 +4,12 @@ using namespace std;
 int main()
 {
     Graph gh;
-    gh.printGraph();
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+        {
+            if (collison[i] && collison[j] && i != j)
+                gh.dijkstra(gh.array[i].place_name, gh.array[j].place_name);
+            cout << endl;
+        }
     return 0;
 }
